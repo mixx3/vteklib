50e85a4415350697db8a9557bb696c4a77dfc1df
Mikhail <mmikee00800@gmail.com>
2022-06-12 00:33:42 +0300

    Update excel_parser.py

--- vteklib/utils/excel_parser.py
+++ vteklib/utils/excel_parser.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 class ExcelFile:
     def __init__(self, excel_fp: str):
-        self.df = pandas.read_excel(excel_fp)
-        self.df = pandas.read_excel('/Users/new/PycharmProjects/vteklib/219.xlsx')
-        self.series = dict()
+        self.df: pd.DataFrame = pandas.read_excel(excel_fp)
+        self.df: pd.DataFrame = pandas.read_excel('/Users/new/PycharmProjects/vteklib/219.xlsx')
+        self.series: dict = dict()
         for col in self.df.columns:
             name = col
             vals = []
@@ -19,9 +19,6 @@ class ExcelFile:
                 curr = self.df[col][pt]
             if vals:
                 self.series[name] = vals
-
-# every excel table contains some columns and its names, so the main issue is to parse all data in table into some
-# pandas Series objs with proper names, and then create a Plot data objs of them (in some way)
 
 
 if __name__ == '__main__':
